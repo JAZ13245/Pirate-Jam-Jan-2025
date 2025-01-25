e204e6bd7a7a660d84007802ed5ba7110fa9218f
James Zilberman <jaz1324576@gmail.com>
2025-01-25 12:28:26 -0500

    Camera Clamp

--- Blink/Assets/Scripts/Player/PlayerCamera.cs
+++ Blink/Assets/Scripts/Player/PlayerCamera.cs
@@ -9,7 +9,10 @@ public class PlayerCamera : MonoBehaviour
     [Header("Camera Settings")]
     [SerializeField] private float sensitvity = 0.1f;
 
-    private Vector3 _eulerAngels;
+    private float InputX;
+    private float InputY;
+    [SerializeField] private float lowerClamp = -90f;
+    [SerializeField] private float upperClamp = 90f;
 
     // Sets Up Player Camera
     public void Initialize(Transform target)
@@ -17,13 +20,19 @@ public class PlayerCamera : MonoBehaviour
         transform.position = target.position;
         transform.rotation = target.rotation;
 
-        transform.eulerAngles = _eulerAngels = target.eulerAngles;
+        //transform.eulerAngles = _eulerAngels = target.eulerAngles;
     }
 
     public void UpdateRotation(CameraInput input)
     {
-        _eulerAngels += new Vector3(-input.Look.y, input.Look.x) * sensitvity;
-        transform.eulerAngles = _eulerAngels;
+        InputX -= input.Look.y * sensitvity;
+        InputY += input.Look.x * sensitvity;
+
+        InputX = Mathf.Clamp(InputX, lowerClamp, upperClamp);
+
+        transform.rotation = Quaternion.Euler(InputX, InputY, 0f);
+
+        //_eulerAngels += new Vector3(InputY, InputX);
     }
     public void UpdatePosition(Transform target)
     {
