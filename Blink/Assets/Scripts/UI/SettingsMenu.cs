ea9e7e1ef76a1ae6e49c31d036e1bbcc55ad9488
John Haley <jah2809@rit.edu>
2025-01-26 05:28:40 -0500

    Main Menu Completed

--- Blink/Assets/Scripts/UI/SettingsMenu.cs
+++ Blink/Assets/Scripts/UI/SettingsMenu.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public Slider volumeSlider;
+    public AudioMixer audioMixer;
+    public float volume;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        volume = PlayerPrefs.GetFloat("Volume", 1f);
+        audioMixer.SetFloat("MasterVolume", GetDecibelVolume(volume));
+
+        // Slider Setting
+        volumeSlider.value = volume;
+
+        this.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume()
     {
-        
+        volume = volumeSlider.value;
+
+        // Set the volume in the mixer
+        audioMixer.SetFloat("MasterVolume", GetDecibelVolume(volume));
+
+        // Save the volume setting
+        PlayerPrefs.SetFloat("Volume", volume);
+        PlayerPrefs.Save();
+
+        Debug.Log("Test?");
+    }
+
+    private float GetDecibelVolume(float volume)
+    {
+        // Convert linear volume (0-1) to logarithmic decibel scale (-80 to 0)
+        float decibelVolume = Mathf.Log10(volume) * 20;
+        if (volume <= 0) decibelVolume = -80; // Mute at 0
+
+        return decibelVolume;
     }
 }
