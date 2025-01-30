5e90256d20efec825c8b91fff593db515e9f1c16
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2025-01-30 03:39:22 -0500

    Removing all my test code

--- Blink/Assets/Scripts/Gun/Gun.cs
+++ Blink/Assets/Scripts/Gun/Gun.cs
@@ -7,8 +7,6 @@ public class Gun : MonoBehaviour
     [SerializeField] private GunType type;
     [SerializeField] private Transform bulletSpawnPoint;
     [SerializeField] private GameObject bullet;
-    [SerializeField] private GameObject bulletTrail;
-    [SerializeField] private LayerMask hitMasks;
     private bool addBulletSpread = false;
     private float shootDelay = 2f;
     private float bulletSpeed = 5f;
@@ -19,10 +17,8 @@ public class Gun : MonoBehaviour
     private Vector3 bulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
 
     private BulletManager bulletManager;
-    TrailRenderer trails;
 
     private Player player;
-    int count = 0;
 
     private enum GunType { pistol, machineGun, shotgun }
 
@@ -35,7 +31,6 @@ public class Gun : MonoBehaviour
     private void Start()
     {
         bulletManager = BulletManager.Instance;
-        trails = bulletTrail.GetComponent<TrailRenderer>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 
         switch (type)
@@ -81,101 +76,6 @@ public class Gun : MonoBehaviour
     public void Shoot(Player player, PlayerCharacter playerBody)
     {
         shoot(player, playerBody);
-    }
-
-    private void TestShoot(Player player, PlayerCharacter playerBody)
-    {
-        if ((lastShootTime + shootDelay < Time.time) && currentAmmo != 0)
-        {
-            /*
-            Vector3 direction = GetDirection(playerBody);
-            
-            if (Physics.Raycast(bulletSpawnPoint.position, direction, out RaycastHit hit, float.MaxValue, hitMasks))
-            {
-                TrailRenderer trail = Instantiate(trails, bulletSpawnPoint.position, Quaternion.identity);
-
-                StartCoroutine(SpawnTrail(trail, hit));
-
-                lastShootTime = Time.time;
-
-                if (currentAmmo > 0)
-                    currentAmmo--;
-            }
-            */
-            
-
-            StartCoroutine(SimulateBullet(playerBody));
-        }
-        else if (currentAmmo == 0)
-        {
-            Invoke("Reload", reloadTime);
-        }
-    }
-
-    private IEnumerator SimulateBullet(PlayerCharacter playerBody)
-    {
-        float step = 0.02f;
-        Vector3 direction = GetDirection(playerBody);
-        Vector3 startPosition = bulletSpawnPoint.position;
-        lastShootTime = Time.time;
-        float travelTime = bulletRange;
-
-        Bullet spawnedBullet = bulletManager.pool.Get();
-        spawnedBullet.transform.position = startPosition;
-
-        if (currentAmmo > 0)
-            currentAmmo--;
-
-        while (travelTime > 0)
-        {
-            travelTime -= step;
-            yield return new WaitForSeconds(step);
-
-            //Debug.DrawLine(startPosition, direction * (bulletSpeed * step));
-
-            if (Physics.Raycast(startPosition, direction, out RaycastHit hit, bulletSpeed * step, hitMasks))
-            {
-                //Debug.Log(hit.collider.tag);
-
-                if (hit.collider.CompareTag("PlayerBody"))
-                {
-                    player.DamagePlayer(bulletDamage);
-                    Debug.Log(hit.collider.bounds);
-                }
-
-                //bulletManager.pool.Release(bullet);
-                break;
-            }
-
-            startPosition += direction * (bulletSpeed * step);
-            spawnedBullet.transform.position = startPosition;
-
-        }
-    }
-
-    private IEnumerator SpawnTrail(TrailRenderer trail, RaycastHit hit)
-    {
-        float time = 0;
-        Vector3 startPosition = trail.transform.position;
-
-        while(time < 1)
-        {
-            trail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
-            time += Time.deltaTime / trail.time;
-            yield return null;
-        }
-        /*
-        //player.DamagePlayer(10);
-        if (hit.collider.CompareTag("PlayerBody"))
-        {
-            Debug.Log("hit player!");
-        }
-        */
-        if (hit.collider.CompareTag("PlayerBody"))
-        {
-            Debug.Log("hit player!");
-        }
-        Destroy(trail.gameObject, trail.time);
     }
 
     private void RegularShoot(Player player, PlayerCharacter playerBody)
